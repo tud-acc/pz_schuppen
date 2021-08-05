6036850f2027d7a18367d46c2ddb32068634ca8f
tud-acc <72494155+tud-acc@users.noreply.github.com>
2021-08-05 13:05:55 +0200

    added files

---
+++ backend.js
@@ -0,0 +1,25 @@
+var server = require("node-fastcgi");
+//var server = require('http');
+var express = require("express");
+var app = express();
+app.set("view engine", "pug");
+app.set("views", "./pug-views");
+app.get("/node.js", function (req, res) {
+  var data = { zahl1: null, zahl2: null };
+  console.log("GET", data);
+  res.render("pug-test", data);
+});
+app.post("/node.js", function (req, res) {
+  var body = "";
+  req.on("data", function (data) {
+    body += data;
+  });
+  req.on("end", function () {
+    var params = new URLSearchParams(body);
+    var data = { zahl1: params.get("zahl1"), zahl2: params.get("zahl2") };
+    //console.dir(params.get('zahl1'),params.get('zahl2');
+    console.log("POST", data);
+    res.render("pug-test", data);
+  });
+});
+server.createServer(app).listen(9998);
