f070a37a8c512d128e2a8a7c796773216b43362f
tud-acc <72494155+tud-acc@users.noreply.github.com>
2021-08-05 13:46:24 +0200

    changed files

--- backend.js
+++ backend.js
@@ -4,6 +4,8 @@ var express = require("express");
 var app = express();
 app.set("view engine", "pug");
 app.set("views", "./pug-views");
+
+//   MAINPAGE (INDEX)
 app.get("/node.js", function (req, res) {
   var data = { zahl1: null, zahl2: null };
   console.log("GET", data);
@@ -22,4 +24,25 @@ app.post("/node.js", function (req, res) {
     res.render("index", data);
   });
 });
+
+//    ANMELDEN
+app.get("/anmelden.js", function (req, res) {
+  var data = { zahl1: null, zahl2: null };
+  console.log("GET", data);
+  res.render("anmelden", data);
+});
+app.post("anmelden.js", function (req, res) {
+  var body = "";
+  req.on("data", function (data) {
+    body += data;
+  });
+  req.on("end", function () {
+    var params = new URLSearchParams(body);
+    var data = { zahl1: params.get("zahl1"), zahl2: params.get("zahl2") };
+    //console.dir(params.get('zahl1'),params.get('zahl2');
+    console.log("POST", data);
+    res.render("index", data);
+  });
+});
+
 server.createServer(app).listen(9998);
