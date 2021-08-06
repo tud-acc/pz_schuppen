e99ee815229b9551296fe6cf83c1fbf0ed934bad
tud-acc <72494155+tud-acc@users.noreply.github.com>
2021-08-06 14:53:22 +0200

    fc

--- backend.js
+++ backend.js
@@ -225,8 +225,7 @@ app.get("/zutaten.js", async function (req, res) {
   var query_sel_zutaten = "SELECT bezeichnung, preis FROM zutaten";
   var result_zutaten = await conn.query(query_sel_zutaten);
   console.log(result_zutaten);
-  var result_data_zutaten = JSON.parse(result_zutaten);
-  //var result_data_zutaten = JSON.stringify(result_zutaten);
+  var result_data_zutaten = JSON.stringify(result_zutaten);
   console.log(result_data_zutaten);
 
   var query_sel_basispizza = "SELECT * FROM pizza";
