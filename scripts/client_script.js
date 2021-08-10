23c734942699c8a148adfc63d9b6d9d675bf8fe2
simant2 <88649667+simant2@users.noreply.github.com>
2021-08-10 15:22:27 +0200

    cf

--- scripts/client_script.js
+++ scripts/client_script.js
@@ -109,6 +109,7 @@ function updatePreSelection(selection) {
       for (let j = 2; j < Object.keys(jobj.Basispizza[i]).length; j++) {
         let currentKey = Object.keys(jobj.Basispizza[i])[j]; //fetched the key at second index
         let zutat = jobj.Basispizza[i][currentKey];
+        console.log("zutat: " + zutat);
         if (zutat !== null && !isNaN(zutat)) {
           checkboxes[zutat - 1].checked = true;
         }
