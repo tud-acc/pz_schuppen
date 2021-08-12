b2cf59e7a7d706eacacbc523e7fe0bd070a4ee75
simant2 <88649667+simant2@users.noreply.github.com>
2021-08-12 11:40:05 +0200

    cf

--- backend.js
+++ backend.js
@@ -30,7 +30,7 @@ app.get("/node.js", function (req, res) {
   console.log("GET - MAINPAGE - FROM:" + req.ip);
   //var ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
   //console.log(ip);
-  var test_session_id = 123456;
+  var test_session_id = "123456";
   /*
   if (Cookies.get("ip") === req.ip) {
     console.dir("Du warst schon mal hier");
