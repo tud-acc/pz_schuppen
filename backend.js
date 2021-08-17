4141522365c0e50a658b505d25a1aa66b2cef376
simant2 <88649667+simant2@users.noreply.github.com>
2021-08-17 10:17:53 +0200

    cf

--- backend.js
+++ backend.js
@@ -368,7 +368,10 @@ async function onMessage(topic, message) {
     let searchedIndex;
     console.log(bestellsession.pizzen);
     for (let i = 0; i < Object.keys(bestellsession.pizzen).length; i++) {
-      if (Number(jsm.pizzaid) === Number(bestellsession.pizzen[i].bestellnr)) {
+      if (
+        bestellsession.pizzen[i] !== null &&
+        jsm.pizzaid === Number(bestellsession.pizzen[i].bestellnr)
+      ) {
         searchedIndex = i;
         break;
       }
