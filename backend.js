c640d996a3edcf97aa54b7f0a3699b7586b0d9b6
simant2 <88649667+simant2@users.noreply.github.com>
2021-08-19 16:05:24 +0200

    cf

--- backend.js
+++ backend.js
@@ -425,7 +425,10 @@ app.post("/bestelluebersicht.js", function (req, res) {
       let query_adress =
         "SELECT strasse, hausnr, plz, ort FROM adresse WHERE adr_id = ?";
       let result_adress = await conn.query(query_adress, [result_kunde.adr_id]);
-      console.log("adr_id" + result_kunde.adr_id);
+
+      console.log("result kunde:");
+      console.log(result_kunde);
+      console.log("adr_id " + result_kunde.adr_id);
       console.log(result_adress);
 
       jsnbestellung.status = 0;
