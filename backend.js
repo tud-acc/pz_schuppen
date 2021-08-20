f34bdf8f8c9df7d3537831f6fbf76f5d5ca3f60a
simant2 <88649667+simant2@users.noreply.github.com>
2021-08-20 16:49:58 +0200

    cf

--- backend.js
+++ backend.js
@@ -751,7 +751,7 @@ async function getBasispizzen() {
   let pizzen = await conn.query("SELECT name FROM pizza");
   console.log(pizzen);
   for (let i = 0; i < Object.keys(pizzen).length; i++) {
-    result[i] = pizzen[i].name;
+    result.push(pizzen[i].name);
   }
   return result;
 }
