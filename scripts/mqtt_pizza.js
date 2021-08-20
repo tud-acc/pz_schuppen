0f2e3ffa55188ce54babdd76afaa0b577db2eb84
simant2 <88649667+simant2@users.noreply.github.com>
2021-08-20 12:27:27 +0200

    cf

--- scripts/mqtt_pizza.js
+++ scripts/mqtt_pizza.js
@@ -4,16 +4,7 @@ var bestellid;
 async function nofuckyou() {
   message = new mqtt_fetch("pizza");
   console.log("message: " + message);
-  //check if http or https:
-  if (location.protocol === "https:") {
-    await message.init(
-      "33972b7a-d9ef-46c1-bfd8-6b830137eaec.ul.bw-cloud-instance.org",
-      8083
-    );
-  } else {
-    await message.init("193.197.231.154", 1884);
-  }
-
+  await message.init("193.197.231.154", 8083);
   message.set_callback(-1, rx_status, false);
   /*document
     .getElementById("addPizzaButton")
