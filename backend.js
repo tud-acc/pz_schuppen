3ff261213ef3854495b0d19000189ab6bbe0efd8
simant2 <88649667+simant2@users.noreply.github.com>
2021-08-24 18:31:22 +0200

    cf

--- backend.js
+++ backend.js
@@ -524,10 +524,9 @@ app.post("/alexa.js", function (req, res) {
 
   req.on("end", async function () {
     // Handle Alexa request
-    console.log(alexa);
     alexa = JSON.parse(alexa);
-    console.log(alexa);
-    console.log(alexa.request.type);
+    console.log(alexa.request);
+    //console.log(alexa.request.type);
 
     // hole session von cache -> undefined wenn session noch nicht existent
     var alexasession = cache.get(alexa.session.sessionId);
